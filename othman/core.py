85e7ceba434c9943ca294ff0de3bf76034e51594
alsadi <alsadi@pc1.ojubabox>
2010-04-22 16:04:31 +0300

    initial commit

---
+++ othman/core.py
@@ -0,0 +1,172 @@
+# -*- coding: UTF-8 -*-
+"""
+Othman - Quran browser
+Core - python module for accessing Othman API
+
+Copyright © 2009-2010, Muayyad Alsadi <alsadi@ojuba.org>
+
+    Released under terms of Waqf Public License.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the latest version Waqf Public License as
+    published by Ojuba.org.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    The Latest version of the license can be found on
+    "http://waqf.ojuba.org/license"
+
+"""
+import sys, os, os.path, time
+import sqlite3
+import struct,zlib
+from itertools import izip
+
+def guessDataDir():
+  if not hasattr(sys, "frozen"):
+    # we are not in py2exe
+    f=os.path.abspath(os.path.dirname(__file__))
+    d=os.path.join(f, '..', 'othman-data')
+    if os.path.exists(d): return d
+    d=(f,'..', '..', '..', '..', 'share', 'othman')
+    if os.path.exists(d): return d
+  # we are in py2exe or DATA can't be located relative to __FILE__
+  f=os.path.abspath(os.path.dirname(sys.argv[0]))
+  d=os.path.join(f, 'othman-data')
+  if os.path.exists(d): return d
+  d=os.path.join(f, '..', 'share', 'othman')
+  return d
+
+class othmanCore:
+  SQL_GET_AYAT='SELECT othmani, imlai FROM Quran WHERE id>=? ORDER BY id LIMIT ?'
+  SQL_GET_SURA_INFO='SELECT rowid, sura_name, other_names, makki, starting_row, comment FROM SuraInfo ORDER BY rowid'
+  def __init__(self):
+    d=guessDataDir()
+    db_fn=os.path.join(d,'quran.db')
+    self.cn=sqlite3.connect(db_fn)
+    self.c=self.cn.cursor()
+    l=list(self.c.execute(self.SQL_GET_SURA_INFO).fetchall())
+    if len(l)!=114: raise IOError
+    self.suraIdByName=dict(((i[1],i[0]) for i in l))
+    self.suraInfoById=[list(i[1:])+[0] for i in l]
+    for i in range(113):
+      self.suraInfoById[i][5]=self.suraInfoById[i+1][3]-self.suraInfoById[i][3]
+    self.suraInfoById[-1][5]=6
+    self.basmala, self.basmala_imlai=list(self.getAyatIter(1))[0]
+    self.basmala=self.basmala[:self.basmala.rfind(' ')]
+
+  def showSunnahBasmala(self, sura):
+    return sura!=1 and sura!=9
+
+  def ayaIdFromSuraAya(self, suraId, aya=1):
+    """
+    suraId: sura number from 1 to 114
+    aya: aya number from 1 to the end of the sura
+    """
+    return self.suraInfoById[suraId-1][3]+aya-1
+
+  def getAyatIter(self, ayaId, number=1):
+    """
+    return a list of (othmani, imlai) tuples
+    """
+    return self.c.execute(self.SQL_GET_AYAT, (ayaId,number))
+
+  def getSuraIter(self, suraId, number=0, fromAya=1):
+    """
+    return a list of (othmani, imlai) tuples for the whole sura
+    suraId: sura number from 1 to 114
+    number of ayat (0 for the whole sura)
+    """
+    a=self.suraInfoById[suraId-1]
+    m=a[5]
+    if number<=0 or number>m: number=m
+    return self.getAyatIter(a[3]+fromAya-1, number)
+
+normalize_tb={
+65: 97, 66: 98, 67: 99, 68: 100, 69: 101, 70: 102, 71: 103, 72: 104, 73: 105, 74: 106, 75: 107, 76: 108, 77: 109, 78: 110, 79: 111, 80: 112, 81: 113, 82: 114, 83: 115, 84: 116, 85: 117, 86: 118, 87: 119, 88: 120, 89: 121, 90: 122,
+1600: None, 1569: 1575, 1570: 1575, 1571: 1575, 1572: 1575, 1573: 1575, 1574: 1575, 1577: 1607, 1611: None, 1612: None, 1613: None, 1614: None, 1615: None, 1616: None, 1617: None, 1618: None, 1609: 1575}
+
+def normalize(s): return s.translate(normalize_tb)
+
+class searchIndexerItem:
+  def __init__(self, ayaId=None, bits=None):
+    if bits:
+      self.bits=bits
+    else:
+      self.bits=[0 for i in range(780)]
+      if ayaId: self.setAyaId(ayaId)
+  
+  def setAyaId(self, ayaId):
+    """
+    ayaId [1-6236]
+    """
+    i=ayaId-1
+    self.bits[(i>>3)]|=(1<<(i&7))
+  
+  def __and__(self, y):
+    return searchIndexerItem(bits=[i and j for (i,j) in izip(self.bits, y.bits)])
+  
+  def toAyaIdList(self):
+    return filter(lambda k: k,
+      sum(map( lambda (i,j):
+      [ (j & 1) and (i<<3)+1,
+        (j & 2) and (i<<3)+2,
+        (j & 4) and (i<<3)+3,
+        (j & 8) and (i<<3)+4,
+        (j & 16) and (i<<3)+5,
+        (j & 32) and (i<<3)+6,
+        (j & 64) and (i<<3)+7,
+        (j & 128) and (i<<3)+8,
+        ]
+      ,filter(lambda (ii,jj): jj,enumerate(self.bits))),[]))
+
+class searchIndexer(dict):
+  def __init__(self, normalize=normalize):
+    dict.__init__(self)
+    self.normalize=normalize
+    self.maxWordLen=0
+  
+  def save(self, fn):
+    s=""
+    m=(self.maxWordLen*2)
+    if os.path.exists(fn): os.unlink(fn)
+    f=open(fn,"wb+")
+    f.write("%d\n" % m)
+    fmt="%ds780B" % m
+    for w in self:
+      a=self[w].bits
+      s+=struct.pack(fmt,w.encode('utf8'),*a)
+    f.write(zlib.compress(s,9))
+    f.close()
+
+  def load(self, fn):
+    f=open(fn,"rb")
+    m=int(f.readline().strip())
+    fmt="%ds780B" % m
+    size=struct.calcsize(fmt)
+    s=zlib.decompress(f.read())
+    for i in range(0,len(s),size):
+      u=struct.unpack(fmt,s[i:i+size])
+      self[u[0].decode('utf8')]=searchIndexerItem(bits=u[1:])
+
+  def find(self, words):
+    if not words: return None
+    w=self.normalize(words[0])
+    x=self.get(w,None)
+    if not x: return None
+    for w in words[1:]:
+      y=self.get(self.normalize(w),None)
+      if not y: return None
+      x&=y
+    return x.toAyaIdList()
+  
+  def addWord(self, word, ayaId):
+    w=self.normalize(word)
+    self.maxWordLen=max(self.maxWordLen,len(w))
+    if self.has_key(w):
+      self[w].setAyaId(ayaId)
+    else:
+      self[w]=searchIndexerItem(ayaId)
+
+
---
+++ othman/gtkUi.py
@@ -0,0 +1,272 @@
+# -*- coding: UTF-8 -*-
+"""
+Othman - Quran browser
+gtkUi - gtk user interface for Othman API
+
+Copyright © 2008-2010, Muayyad Alsadi <alsadi@ojuba.org>
+
+    Released under terms of Waqf Public License.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the latest version Waqf Public License as
+    published by Ojuba.org.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    The Latest version of the license can be found on
+    "http://waqf.ojuba.org/license"
+
+"""
+import sys, os, os.path, time
+import gettext
+import pango
+import glib
+import gtk
+
+from core import othmanCore
+
+class othmanUi(gtk.Window, othmanCore):
+  def __init__(self):
+    gtk.window_set_default_icon_name('othman')
+    gtk.Window.__init__(self)
+    othmanCore.__init__(self)
+    self.set_title(_('Othman Quran Browser'))
+    self.connect("delete_event", self.quit)
+    self.set_default_size(600, 480)
+
+    self.clip1=gtk.Clipboard(selection="PRIMARY")
+    self.clip2=gtk.Clipboard(selection="CLIPBOARD")
+    self.accel=gtk.AccelGroup()
+
+    vb=gtk.VBox(False,0); self.add(vb)
+    hb=gtk.HBox(False,2)
+    vb.pack_start(hb,False, False, 0)
+
+    self.scroll=gtk.ScrolledWindow()
+    self.scroll.set_policy(gtk.POLICY_NEVER,gtk.POLICY_ALWAYS)
+    self.scroll.connect_after("size-allocate", self.resize_cb)
+    vb.pack_start(self.scroll,True, True, 6)
+
+    img=gtk.Image()
+    img.set_from_stock(gtk.STOCK_COPY,gtk.ICON_SIZE_BUTTON)
+    b=gtk.Button()
+    b.add(img)
+    b.connect("clicked", self.show_cp_dlg)
+    hb.pack_start(b,False, False, 0)
+    hb.pack_start(gtk.VSeparator(),False, False, 6)
+    hb.pack_start(gtk.Label(_("Sura")),False, False, 0)
+
+    self.sura_ls=tuple("%d. %s" % (i+1,j[0]) for (i,j) in enumerate(self.suraInfoById))
+    self.sura_c = gtk.combo_box_new_text()
+    self.sura_c.set_wrap_width(5)
+    for i in self.sura_ls: self.sura_c.append_text(i)
+    self.sura_c.set_tooltip_text(_("choose a Sura"))
+    self.sura_c.connect("changed", self.sura_changed_cb)
+    hb.pack_start(self.sura_c,False, False, 0)
+
+    hb.pack_start(gtk.VSeparator(),False, False, 6)
+    img=gtk.Image()
+    img.set_from_stock(gtk.STOCK_ZOOM_IN, gtk.ICON_SIZE_BUTTON)
+    b=gtk.Button()
+    b.add(img)
+    hb.pack_start(b, False, False, 0)
+    b.connect("clicked", self.zoomIn)
+    
+    img=gtk.Image()
+    img.set_from_stock(gtk.STOCK_ZOOM_OUT, gtk.ICON_SIZE_BUTTON)
+    b=gtk.Button()
+    b.add(img)
+    hb.pack_start(b, False, False, 0)
+    b.connect("clicked", self.zoomOut)
+
+    img=gtk.Image()
+    img.set_from_stock(gtk.STOCK_MEDIA_FORWARD, gtk.ICON_SIZE_BUTTON)
+    b=gtk.ToggleButton()
+    b.add(img)
+    hb.pack_start(b, False, False, 0)
+    self.autoScrolling=False
+    b.connect("clicked", self.autoScrollCb)
+    glib.timeout_add(100, self.autoScroll, b)
+
+    hb.pack_start(gtk.VSeparator(), False, False, 6)
+    hb.pack_start(gtk.image_new_from_stock(gtk.STOCK_FIND, gtk.ICON_SIZE_BUTTON), False, False, 0)
+    inc_search=gtk.Entry(); inc_search.set_width_chars(15)
+    hb.pack_start(inc_search, False,False, 0)
+    self.inc_is_backword=gtk.CheckButton(_("backword search"))
+    hb.pack_start(self.inc_is_backword, False, False, 0)
+    #inc_search.connect("activate", self.inc_search_cb)
+    self.scale=1
+    self.txt = gtk.ListStore(str,int,str)
+    self.cells=[]; self.cols=[]
+    self.cells.append(gtk.CellRendererText())
+    #self.cols.append(gtk.TreeViewColumn('Quranic Text', self.cells[0], markup=0))
+    self.cols.append(gtk.TreeViewColumn('Quranic Text', self.cells[0], text=0, foreground=2))
+    self.cells[0].set_property("background","#fffff8")
+    #self.cells[0].set_property("foreground","#204000")
+    #self.cells[0].set_property("alignment",pango.ALIGN_CENTER)
+    self.cells[0].set_property("wrap-mode",pango.WRAP_WORD_CHAR)
+    self.cells[0].set_property("wrap-width",500)
+    self.cells[0].set_property("font","Simplified Naskh 32")
+    self.cells[0].set_property("scale", self.scale)
+    self.cols[0].set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+
+    self.cells.append(gtk.CellRendererText()); # self.cols[-1].set_expand(False)
+    self.txt_list=gtk.TreeView(self.txt)
+    self.txt_list.set_headers_visible(False)
+    for i in self.cols: self.txt_list.insert_column(i, -1)
+
+    self.scroll.add(self.txt_list)
+    self.sura_c.set_active(0)
+    self.build_cp_dlg()
+    self.show_all()
+
+  def autoScroll(self, b):
+    if not self.autoScrolling: return True
+    v=self.scroll.get_vadjustment()
+    m=v.upper-v.page_size
+    n=min(m, v.get_value() + 2 )
+    if n==m: b.set_active(False)
+    v.set_value(n)
+    return True
+
+  def autoScrollCb(self, b, *a):
+    self.autoScrolling=b.get_active()
+
+  def zoomIn(self, *a):
+    sura,aya=self.getCurrentSuraAya()
+    self.scale+=0.1
+    self.cells[0].set_property("scale", self.scale)
+    self.resize_cb()
+    self.queue_draw()
+    self.viewSura(sura)
+    self.viewAya(aya)
+
+  def zoomOut(self, *a):
+    sura,aya=self.getCurrentSuraAya()
+    self.scale-=0.1
+    self.scale=max(0.2, self.scale)
+    self.cells[0].set_property("scale", self.scale)
+    self.resize_cb()
+    self.queue_draw()
+    self.viewSura(sura)
+    self.viewAya(aya)
+
+  def viewAya(self, aya, sura=None):
+    if sura==None: sura=self.sura_c.get_active()+1
+    aya=max(1,abs(aya))
+    i=aya+int(self.showSunnahBasmala(sura))
+    self.txt_list.scroll_to_cell((i-1,))
+    self.txt_list.get_selection().select_path((i-1,))
+
+
+  def viewSura(self, i):
+    #self.play_pause.set_active(False)
+    self.txt.clear()
+    if self.showSunnahBasmala(i):
+      #self.txt.append(['<span foreground="#440000">%s</span>' % self.basmala,0,])
+      self.txt.append([self.basmala,0,"#802000",])
+    for j,k in enumerate(self.getSuraIter(i)):
+      self.txt.append([k[0],j+1,"#204000",])
+    self.resize_cb()
+    self.scroll.get_vadjustment().set_value(0)
+    self.txt_list.get_selection().select_path((0,))
+
+  def sura_changed_cb(self, c, *a):
+    self.viewSura(self.sura_c.get_active()+1)
+
+  def resize_cb(self,*args):
+    if self.cols[0].get_width()>10: self.cells[0].set_property("wrap-width",self.cols[0].get_width()-10)
+
+  def cp_cb(self, *a):
+    sura=self.cp_sura.get_active()+1
+    aya1=self.cp_from.get_value()
+    aya2=self.cp_to.get_value()
+    n=aya2-aya1+1
+    i=self.cp_is_imlai.get_active()
+    a=['','\n',' * ', ' *\n']
+    s=a[int(i)*2+int(self.cp_aya_perline.get_active())]
+    s=s.join([l[i] for l in self.getSuraIter(sura, n, aya1)])+'\n'
+    self.clip1.set_text(s)
+    self.clip2.set_text(s)
+    self.cp_w.hide()
+
+  def cp_sura_cb(self, *a):
+    sura=self.cp_sura.get_active()+1
+    m=self.suraInfoById[sura-1][5]
+    self.cp_from.set_range(1, m)
+    self.cp_to.set_range(1, m)
+    self.cp_from.set_value(1)
+    self.cp_to.set_value(m)
+
+  def show_cp_dlg(self, *a):
+    sura,aya=self.getCurrentSuraAya()
+    aya=max(1, abs(aya))
+    self.cp_sura.set_active(sura-1)
+    self.cp_sura_cb()
+    self.cp_from.set_value(aya)
+    self.cp_w.show_all()
+
+  def build_cp_dlg(self):
+    self.cp_w = gtk.Window()
+    self.cp_w.set_title(_('Copy to clipboard'))
+    self.cp_w.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+    self.cp_w.connect('delete-event', lambda w,*a: w.hide() or True)
+    self.cp_sura = gtk.combo_box_new_text(); self.cp_sura.set_wrap_width(5)
+
+    for i in self.sura_ls: self.cp_sura.append_text(i)
+    self.cp_sura.set_tooltip_text(_("choose a Sura"))
+    
+    self.cp_from=gtk.SpinButton()
+    self.cp_to=gtk.SpinButton()
+    self.cp_is_imlai=gtk.CheckButton("بالرسم الإملائي")
+    self.cp_aya_perline=gtk.CheckButton("كل آية على سطر")
+    self.cp_ok=gtk.Button(stock=gtk.STOCK_OK)
+    self.cp_cancel=gtk.Button(stock=gtk.STOCK_CANCEL)
+    vb=gtk.VBox(False,0)
+    self.cp_w.add(vb)
+    hb=gtk.HBox(False,3)
+    vb.pack_start(hb,True,True,3)
+    hb.pack_start(gtk.Label("سورة"),False,False,3)
+    hb.pack_start(self.cp_sura,False,False,6)
+    hb=gtk.HBox(False,6)
+    vb.pack_start(hb,True,True,6)
+    hb.pack_start(gtk.Label("الآيات من"),False,False,3)
+    hb.pack_start(self.cp_from,False,False,3)
+    hb.pack_start(gtk.Label("إلى"),False,False,3)
+    hb.pack_start(self.cp_to,False,False,3)
+    hb=gtk.HBox(False,6)
+    vb.pack_start(hb,True,True,6)
+    hb.pack_start(self.cp_is_imlai,False,False,3)
+    hb.pack_start(self.cp_aya_perline,False,False,3)
+    hb=gtk.HBox(False,6)
+    vb.pack_start(hb,True,True,6)
+    hb.pack_start(self.cp_ok,False,False,6)
+    hb.pack_start(self.cp_cancel,False,False,6)
+    self.cp_sura.connect("changed", self.cp_sura_cb)
+    self.cp_cancel.connect('clicked', lambda *args: self.cp_w.hide())
+    self.cp_ok.connect('clicked', self.cp_cb)
+    self.cp_is_imlai.set_active(True)
+
+  def getCurrentSuraAya(self):
+    a=self.txt_list.get_selection().get_selected()
+    aya=1
+    if a: aya=self.txt[self.txt.get_path(a[1])[0]][1]
+    aya=max(aya,1)
+    return self.sura_c.get_active()+1, aya
+
+  def quit(self,*args):
+     gtk.main_quit()
+     return False
+
+def main():
+  exedir=os.path.dirname(sys.argv[0])
+  ld=os.path.join(exedir, 'locale')
+  if not os.path.exists(ld): ld=os.path.join(exedir,'..','share','locale')
+  gettext.install('othman', ld, unicode=0)
+  w=othmanUi()
+  gtk.main()
+
+if __name__ == "__main__":
+  main()
+
