5bb7838c19fc850e71f11754fa36858aae66d4fe
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2011-05-27 15:15:44 +0300

    more compact varuints

--- othman/varuints.py
+++ othman/varuints.py
@@ -22,13 +22,19 @@ based on idea from http://code.google.com/apis/protocolbuffers/docs/encoding.htm
 
 but unlike protocolbuffers it preserve order by saving most significant first
 
-in this implementation a single
-varuint can be something like
+in this implementation a single varuint can be something like
 
-1bbb-bbbb 1bbb-bbbb 0bbb-bbbb
+1xxx-xxxx 1xxx-xxxx 0xxx-xxxx
 
 MSB in each byte is "has_more", and rest bits are base-128 int
-the above is bbb-bbbb bbb-bbbb bbb-bbbb
+the above is xxx-xxx xxx-xxxx xxx-xxxx
+
+
+0xxx-xxxx is 0-127
+
+1xxx-xxxx 0xxx-xxxx  is 128-16511
+
+and so on
 
 use it like this
 
@@ -41,46 +47,76 @@ def decode(s):
   v=0
   for c in s:
     c=ord(c)
-    v<<=7
-    v+=c&127
+    v+=(c&127)
     if (c&128)==0: yield v; v=0
-  if v: raise
+    else: v+=1
+    v<<=7
+  if v: raise ValueError
 
 def encode(a):
   r=[]
   k=0
   for i in a:
     j,m=0,0
-    while(i>0):
+    while(1):
       r.insert(k,chr(m|(i&127)))
-      i>>=7
       m=128
       j+=1
-    if j==0: r.insert(k,'\0'); k+=1
-    else: k+=j
+      i>>=7
+      if i==0: break
+      i-=1
+    k+=j
   return "".join(r)
 
-def incremetal_encode_list(a, unique=1):
-  last=0
+def write(f, a):
+  """encode members of a and write them to file f and return the number of bytes written"""
+  s=encode(a)
+  f.write(s)
+  return len(s)
+
+def read(f, limit=1):
+  """read and decode up to limit integers from file f and return them"""
+  v,n=0,0
+  while(n<limit):
+    c=f.read(1)
+    if c=='': raise ValueError
+    c=ord(c)
+    v+=(c&127)
+    if (c&128)==0: yield v; v=0; n+=1
+    else: v+=1
+    v<<=7
+  if v: raise ValueError
+
+def read_single(f):
+  """read and decode a single varuint from file f and return it"""
+  return read(f,1).next()
+
+
+def incremetal_encode_list(a, unique=1, last=0):
   if unique!=1 and unique!=0: raise ValueError
   for i in a:
     if i<last+unique: raise ValueError
     yield i-last-unique
     last=i
 
-def incremetal_decode_list(a, unique=1):
-  last=0
+def incremetal_decode_list(a, unique=1, last=0):
   if unique!=1 and unique!=0: raise ValueError
   for i in a:
     j=i+last+unique
     yield j
     last=j
  
-def incremetal_encode(a, unique=1):
-  return encode(incremetal_encode_list(a, unique))
+def incremetal_encode(a, unique=1, last=0):
+  return encode(incremetal_encode_list(a, unique, last))
 
-def incremetal_decode(s, unique=1):
-  return incremetal_decode_list(decode(s), unique)
+def incremetal_decode(s, unique=1, last=0):
+  return incremetal_decode_list(decode(s), unique, last)
+
+def incremetal_write(f, a, unique=1, last=0):
+  return write(f, incremetal_encode_list(a, unique, last))
+
+def incremetal_read(f, unique=1, limit=1, last=0):
+  return incremetal_decode_list(read(f, limit), unique, last)
 
 
 def decode_safe(s):
